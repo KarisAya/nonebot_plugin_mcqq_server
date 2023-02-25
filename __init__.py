0fe1389195b028b31141800a5d49af3022fa248f
KarisAya <1048827424@qq.com>
2023-02-25 19:43:50 +0800

    重连时清空msr

--- __init__.py
+++ __init__.py
@@ -59,24 +59,25 @@ if log.exists():
         with open(log, "r", encoding = "utf8") as fp:
             pos = fp.seek(0,2)
         while True:
-            fp = open(log, "r", encoding = "utf8")
-            fp.seek(pos, 0)
-            line = fp.read()
-            if line:
-                pos = fp.seek(0,2)
-                if switch:
+            if switch == False:
+                await asyncio.sleep(1)
+                continue
+            else:
+                fp = open(log, "r", encoding = "utf8")
+                fp.seek(pos, 0)
+                if line := fp.read():
+                    pos = fp.seek(0,2)
                     line.replace("\r\n","\n")
                     line = line.strip('\n').split('\n')
                     for x in line:
-                        msg_dict = log_to_dict(x)
-                        if msg_dict:
+                        if msg_dict := log_to_dict(x):
                             msg = f'【{msg_dict["nickname"]}】{msg_dict["message"]}'
                             for group in group_list:
                                 await bot.send_group_msg(group_id = group,message = msg)
                             for guild in guild_list:
                                 await bot.send_guild_channel_msg(guild_id = guild['guild_id'],channel_id = guild['channel_id'],message = msg)
-            fp.close()
-            await asyncio.sleep(1)
+                fp.close()
+                await asyncio.sleep(1)
 else:
     logger.error(f"mc_log_path 地址设置错误，{log} 不存在。")
 
@@ -85,24 +86,21 @@ else:
 async def CUSTOMER(event: MessageEvent) -> bool:
     if isinstance(event,GroupMessageEvent):
         return event.group_id in group_list
-    elif isinstance(event,GuildMessageEvent):
+    if isinstance(event,GuildMessageEvent):
         for guild in guild_list:
             if event.guild_id == guild["guild_id"] and event.channel_id == guild["channel_id"]:
                 return True
         else:
             return False
-    else:
-        return False
-
-mcr = asyncio.run(mcrcon_connect("127.0.0.1", mcrcon_password, mcrcon_port))
+    return False
 
 flag = True
 
+mcr = asyncio.run(mcrcon_connect("127.0.0.1", mcrcon_password, mcrcon_port))
 async def check():
     return switch and (mcr or flag)
 
 send = on_message(rule = check, permission = CUSTOMER, priority = 10)
-
 @send.handle()
 async def _(bot: Bot, event: Event):
     global flag, mcr
@@ -111,5 +109,6 @@ async def _(bot: Bot, event: Event):
         mcr.command(msg)
         flag = True
     except:
-        mcr = await mcrcon_connect("127.0.0.1", mcrcon_password, mcrcon_port)
         flag = False
+        mcr = None
+        mcr = await mcrcon_connect("127.0.0.1", mcrcon_password, mcrcon_port)
